d12ad76cec1b5edab69edbf11d760541d658a8ff
siqister <siqi.zhu@gmail.com>
2018-01-14 16:07:27 -0500

    Add /trip api end points

--- models/Trip.js
+++ models/Trip.js
@@ -2,20 +2,31 @@ const mongoose = require('mongoose');
 
 const TripSchema = new mongoose.Schema({
 	"tripduration" : Number,
-	//"starttime" : Number, //ought to be date
-	//"stoptime" : Number,
-	"start station id" : {type:Number, default:0},
-	"start station name" : String,
-	"start station latitude" : Number,
-	"start station longitude" : Number,
-	"end station id" : {type:Number, default:0},
-	"end station name" : String,
-	"end station latitude" : Number,
-	"end station longitude" : Number,
+	"starttime" : Date,
+	"stoptime" : Date,
+	"start_station_id" : String,
+	"start_station_name" : String,
+	"start_station_latitude" : Number,
+	"start_station_longitude" : Number,
+	"end_station_id" : String,
+	"end_station_name" : String,
+	"end_station_latitude" : Number,
+	"end_station_longitude" : Number,
 	"bikeid" : Number,
 	"usertype" : Number,
 	"birth year" : Number,
 	"gender" : Number
-},{timestamps:true});
+},{timestamps:true},{collection:'trips'});
+
+TripSchema.methods.toJSON = function(){
+	return {
+		duration:this.tripduration,
+		station0:this['start_station_id'],
+		station1:this['end_station_id'],
+		bike_nr:this.bikeid,
+		t0:this.starttime,
+		t1:this.stoptime
+	}
+}
 
 mongoose.model('Trip',TripSchema);
--- routes/api/trips.js
+++ routes/api/trips.js
@@ -4,8 +4,44 @@ const Trip = mongoose.model('Trip');
 
 router.get('/', (req,res,next) => {
 
-	res.json({
-		message:'Hello world'
+	const query = {};
+	const stop1 = req.query.stop1;
+	const stop2 = req.query.stop2;
+	const t0 = req.query.t0;
+	const t1 = req.query.t1;
+
+	//Query by stops
+	if(stop1 && stop2){
+		query.$or = [
+			{start_station_id:String(stop1), end_station_id:String(stop2)},
+			{start_station_id:String(stop2), end_station_id:String(stop1)}
+		];
+	}else if(stop1){
+		query.$or = [
+			{start_station_id: String(stop1)},
+			{end_station_id: String(stop1)}
+		];
+	}
+
+	//Query by date
+	if(t0){
+		query.starttime = {$gte:new Date(+t0)};
+	}
+	if(t1){
+		query.stoptime = {$lte:new Date(+t1)};
+	}
+
+	return Promise.all([
+		Trip.find(query),
+		Trip.count(query).exec()
+	]).then(function(results){
+		const trips = results[0];
+		const count = results[1];
+
+		return res.json({
+			count:count,
+			trips:trips.map(x => x.toJSON())
+		});
 	});
 
 });
